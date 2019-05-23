0a9e6d5eb3142bf638c69d32afa9b2cb87ce2545
Hélder Pestana <hgpestana@gmail.com>
2019-05-23 14:19:50 +0100

    Updated the ApiTokenAuthenticator

--- Security/Core/Authentication/ApiTokenAuthenticator.php
+++ Security/Core/Authentication/ApiTokenAuthenticator.php
@@ -1,8 +1,15 @@
 <?php
 
+/*
+ * This file is part of hgpestana's user bundle.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace HGPestana\UserBundle\Security\Core\Authentication;
 
+use Exception;
 use HGPestana\UserBundle\Entity\ApiToken;
 use HGPestana\UserBundle\Entity\User;
 use HGPestana\UserBundle\Repository\ApiTokenRepository;
@@ -15,6 +22,12 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 
+/**
+ * Responsible for providing basic API authentication using
+ * the API token entity defined in the bundle.
+ *
+ * @author Hélder Pestana <hgpestana@gmail.com>
+ */
 class ApiTokenAuthenticator extends AbstractGuardAuthenticator
 {
     private const INVALID_CREDENTIALS_ERROR = 'Invalid credentials supplied! Please try again.';
@@ -23,19 +36,31 @@ class ApiTokenAuthenticator extends AbstractGuardAuthenticator
     /** @var ApiTokenRepository */
     private $apiTokenRepository;
 
+    /**
+     * {@inheritDoc}
+     */
     public function __construct(ApiTokenRepository $apiTokenRepository)
     {
         $this->apiTokenRepository = $apiTokenRepository;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function start(Request $request, AuthenticationException $authException = null)
     {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function supports(Request $request): ?bool
     {
         // look for header "Authorization: Bearer <token>"
@@ -43,39 +68,57 @@ class ApiTokenAuthenticator extends AbstractGuardAuthenticator
             && 0 === strpos($request->headers->get('Authorization'), 'Bearer ');
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function getCredentials(Request $request): ?string
     {
         $authorizationHeader = $request->headers->get('Authorization');
         return substr($authorizationHeader, 7);
     }
 
+    /**
+     * {@inheritDoc}
+     * @throws Exception
+     */
     public function getUser($credentials, UserProviderInterface $userProvider): ?User
     {
         /** @var ApiToken $token */
         $token = $this->apiTokenRepository->findOneByToken($credentials);
 
-        if (!$token)
+        if (!$token) {
             throw new CustomUserMessageAuthenticationException(self::INVALID_CREDENTIALS_ERROR);
+        }
 
-        if ($token->isExpired())
+        if ($token->isExpired()) {
             throw new CustomUserMessageAuthenticationException(self::TOKEN_EXPIRED_ERROR);
+        }
 
         return $token->getUser();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
         return new JsonResponse([
-            'message' => $exception->getMessageKey()
+            'message' => $exception->getMessageKey(),
         ], 401);
     }
 
-    public function checkCredentials($credentials, UserInterface $user)
+    /**
+     * {@inheritDoc}
+     */
+    public function checkCredentials($credentials, UserInterface $user): bool
     {
         return true;
     }
 
-    public function supportsRememberMe()
+    /**
+     * {@inheritDoc}
+     */
+    public function supportsRememberMe(): bool
     {
         return false;
     }
